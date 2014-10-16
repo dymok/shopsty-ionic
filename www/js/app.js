bf582930592aac8ea517a7fa85576961c64d75e6
Sergey Koval <s.koval@smile2mobile.net>
2014-10-16 15:42:27 +0300

    Initial layout with left side menu

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,13 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('shopsty', ['ionic'])
+
+.controller('AppController', function($scope, $ionicSideMenuDelegate) {
+  $scope.toggleMenu = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
